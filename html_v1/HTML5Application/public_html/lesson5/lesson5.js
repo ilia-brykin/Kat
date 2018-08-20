666aadf44f3a73cf59410b11eb11c8f75303e02d
Ilia Brykin <ilia.brykin@prounix.de>
2018-08-20 08:46:16 +0200

    Lesson 5 korrigiert

--- html_v1/HTML5Application/public_html/lesson5/lesson5.js
+++ html_v1/HTML5Application/public_html/lesson5/lesson5.js
@@ -1,71 +1,63 @@
-function loadAlbum(path)
-{
-    var iframe = document.getElementById('target');
-    iframe.setAttribute('src', path);
-    sessionStorage["album"] = path;
-}
-function addtoPL(name, path)
-{
-    localStorage['./../../../../audio/slot/1.mp3'] = path; /*добавляем в localStorage путь к аудио файлу*/
-    window.parent.location.href = window.parent.location.href;
+// function loadAlbum(path) {
+//   var iframe = document.getElementById('target');
+//   iframe.setAttribute('src', path);
+//   sessionStorage["album"] = path;
+// }
+
+function addtoPL(name, path) {
+  localStorage[name] = path;
+  /*добавляем в localStorage путь к аудио файлу*/
+  window.parent.location.href = window.parent.location.href;
 }
 
-function testListen(path)
-{
-    var audio = document.createElement('audio');
-    var div = document.getElementById('tracks');
-    audio.src = path;
-    audio.controls = false;
-    div.appendChild(audio);
-    audio.addEventListener('timeupdate', function ()
-    {
-        if (audio.currentTime > 10)
-        {
-            audio.pause();
-        }
-    }, false); /*так переносить можно?*/
-    audio.play();
-}
-function playtrack(track)
-{
-    var b = false;
-    for (var i in localStorage)
-    {
-        if (b) {
-            localStorage['next'] = localStorage[i];
-            break;
-        }
-        if (i === track) {
-            b = true;
-        }
+function testListen(path) {
+  var audio = document.createElement('audio');
+  var div = document.getElementById('tracks');
+  audio.src = path;
+  audio.controls = false;
+  div.appendChild(audio);
+  audio.addEventListener('timeupdate', function () {
+    if (audio.currentTime > 10) {
+      audio.pause();
     }
-    var audio = document.getElementById('audio');
-    audio.src = localStorage[track];
-    audio.controls = true;
-    audio.play();
+  }, false);
+  /*так переносить можно?*/
+  audio.play();
 }
 
-function load()
-{
-    if (sessionStorage["album"] != undefined)
-    {
-        loadAlbum(sessionStorage["album"]);
+function playtrack(track) {
+  var b = false;
+  for (var i in localStorage) {
+    if (b) {
+      localStorage['next'] = localStorage[i];
+      break;
     }
-    var audio = document.getElementById('audio');
-    audio.addEventListener('ended', function ()
-    {
-        playtrack('next');
-    }, false);
-    var list = document.getElementById('playlist');
-    for (var i in localStorage)
-    {
-        var element = document.createElement('li');
-        element.setAttribute('class', 'PL');
-        element.setAttribute('onclick', "playtrack('" + i + "')");
-        element.innerText = i;
-        if (i != 'next')
-        {
-            list.appendChild(element);
-        }
+    if (i === track) {
+      b = true;
     }
+  }
+  var audio = document.getElementById('audio');
+  audio.src = localStorage[track];
+  audio.controls = true;
+  audio.play();
+}
+
+function load() {
+  //if (sessionStorage["album"] != undefined) {
+  //  loadAlbum(sessionStorage["album"]);
+  //}
+  // var audio = document.getElementById('audio');
+  // audio.addEventListener('ended', function () {
+  //   playtrack('next');
+  // }, false);
+  var list = document.getElementById('playlist');
+  for (var i in localStorage) {
+    var element = document.createElement('li');
+    element.setAttribute('class', 'PL');
+    element.setAttribute('onclick', "playtrack('" + i + "')");
+    element.innerText = i;
+    if (i != 'next') {
+      list.appendChild(element);
+    }
+  }
 }
