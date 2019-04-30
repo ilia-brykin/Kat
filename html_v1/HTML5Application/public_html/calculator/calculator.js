31bb1d4ee716281ffd058771f56292759670857f
Katz1313 <e.kapran@mail.ru>
2019-04-30 20:09:34 +0300

    30.04.19(calculator)

---
+++ html_v1/HTML5Application/public_html/calculator/calculator.js
@@ -0,0 +1,7 @@
+var div = document.querySelector('.result');
+console.log(div.number); // нужно заменить console.log на что-то
+div.number = buttonsNumber;
+var buttonsNumber = function (number){
+
+};
+
