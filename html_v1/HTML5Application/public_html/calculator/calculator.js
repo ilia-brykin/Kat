d3a6fe0a219300ac90989791f15155ab7a076b61
Katz1313 <e.kapran@mail.ru>
2019-05-08 22:24:31 +0300

    2--08.05.19(calculator)

--- html_v1/HTML5Application/public_html/calculator/calculator.js
+++ html_v1/HTML5Application/public_html/calculator/calculator.js
@@ -2,13 +2,29 @@ var globalResult = document.querySelector('#result');
 var globalNumber = "";
 console.log("globalResult", globalResult);
 
-// div.number = buttonsNumber;
-// var buttonsNumber = function (number){
-//
-// };
-
 
 function onButtonsNumber(number) {
   globalNumber += number;
   globalResult.innerText = globalNumber;
+}
+
+function onButtonsSymbol(number) {
+    var number2 = "";
+    number2 += number;
+    globalResult.innerText = number2;
+    
+    if ('+') {
+      globalNumber += number2; 
+    }
+    else if ('-') {
+      globalNumber -= number2; 
+    }
+    else if ('*') {
+      globalNumber *= number2; 
+    }
+    else if ('/') {
+      globalNumber /= number2; 
+    }
+    else if ('=') {
+    }
 }
