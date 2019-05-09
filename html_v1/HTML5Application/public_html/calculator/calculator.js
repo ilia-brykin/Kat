9ae1b3c90ba991e29cc8567acb9758bc9e8251cb
Ilia Brykin <ilia.brykin@prounix.de>
2019-05-09 15:29:37 +0200

    Result Funktion für calc

--- html_v1/HTML5Application/public_html/calculator/calculator.js
+++ html_v1/HTML5Application/public_html/calculator/calculator.js
@@ -3,28 +3,39 @@ var globalNumber = "";
 console.log("globalResult", globalResult);
 
 
-function onButtonsNumber(number) {
-  globalNumber += number;
+function onButtonsSymbol(symbol) {
+  globalNumber += symbol;
   globalResult.innerText = globalNumber;
+    // var number2 = "";
+    // number2 += number;
+    // globalResult.innerText = number2;
+    //
+    // if ('+') {
+    //   globalNumber += number2;
+    // }
+    // else if ('-') {
+    //   globalNumber -= number2;
+    // }
+    // else if ('*') {
+    //   globalNumber *= number2;
+    // }
+    // else if ('/') {
+    //   globalNumber /= number2;
+    // }
 }
 
-function onButtonsSymbol(number) {
-    var number2 = "";
-    number2 += number;
-    globalResult.innerText = number2;
-    
-    if ('+') {
-      globalNumber += number2; 
-    }
-    else if ('-') {
-      globalNumber -= number2; 
-    }
-    else if ('*') {
-      globalNumber *= number2; 
-    }
-    else if ('/') {
-      globalNumber /= number2; 
-    }
-    else if ('=') {
-    }
+function getResult() {
+  var tempNumber;
+  try {
+    tempNumber = eval(globalNumber);
+    // код ...
+
+  } catch (error) {
+    console.log("error", error);
+    // обработка ошибки
+    return;
+  }
+  globalNumber = tempNumber;
+  globalResult.innerText = globalNumber;
+  console.log("globalNumber", );
 }
